5fa1a90c401c1f2e735245a1d104f82b3794b875
ArturDykin <dykin.artur@mail.ru>
2024-02-14 15:59:35 +0300

    variable calculation fix

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число:");
+        System.out.println("Введите первое число:");
         int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
         int secondNumber = new Scanner(System.in).nextInt();
         double x = (double) (firstNumber / secondNumber);
         System.out.print("сумма: " + (firstNumber + secondNumber) +"\n" + "разность:  " + (firstNumber - secondNumber)+"\n" + "произведение:  " + (firstNumber - secondNumber)+"\n" + "часное:  " + x);
