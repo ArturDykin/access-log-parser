cc2ff1eab250b9f8fbb6e7b505199c18dfaf8773
ArturDykin <dykin.artur@mail.ru>
2024-03-06 11:26:19 +0300

    Этоговая курсовая работа по циклам

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int count = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists && !isDirectory) {
+                System.out.printf("Путь указан верно. Это файл номер %d" ,++count);
+            } else {
+                System.out.printf("Путь к файлу не правильный ");
+                continue;
+            }
+        }
     }
 }
